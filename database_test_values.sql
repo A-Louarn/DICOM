4cf876cf611462399b9011c5f444bbe16ebcbf1a
TheMrNomis <n0m1s@homnomnom.fr>
2015-08-28 23:16:35 +0200

    added a few test values to populate the database

---
+++ database_test_values.sql
@@ -0,0 +1,14 @@
+-- Operateur
+INSERT INTO Operateur VALUES('Dr. Zoidberg');
+INSERT INTO Operateur VALUES('Dr. Frankenstein');
+INSERT INTO Operateur VALUES('Dr. Strange');
+
+-- Realisateur
+INSERT INTO Realisateur VALUES('Dr. Cooper');
+INSERT INTO Realisateur VALUES('Dr. Lecter');
+INSERT INTO Realisateur VALUES('Dr. House');
+
+-- Prescripteur
+INSERT INTO Prescripteur VALUES('Dr. Watson');
+INSERT INTO Prescripteur VALUES('Dr. Jekyll');
+INSERT INTO Prescripteur VALUES('Dr. McCoy');
