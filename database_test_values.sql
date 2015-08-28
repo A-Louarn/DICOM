a676b477367c7d770e5a8f63f8c8985c55ca5a7c
TheMrNomis <n0m1s@homnomnom.fr>
2015-08-29 00:24:46 +0200

    added some test values to the Patient table

--- database_test_values.sql
+++ database_test_values.sql
@@ -12,3 +12,6 @@ INSERT INTO Realisateur VALUES('Dr. House');
 INSERT INTO Prescripteur VALUES('Dr. Watson');
 INSERT INTO Prescripteur VALUES('Dr. Jekyll');
 INSERT INTO Prescripteur VALUES('Dr. McCoy');
+
+-- Patient
+INSERT INTO Patient (patient_insee, patient_firstName, patient_lastName, patient_dateOfBirth, patient_sex, patient_size, patient_weight, patient_typeOfID, patient_adress, patient_insurancePlanIdentification, patient_countryOfResidence, patient_telephoneNumber, patient_additionalHistory) VALUES ('0', 'Bruce', 'Wayne', '2015-01-13', 'M', '180', '90', 'INSEE', 'Manor Wayne, Gotham City', 'INSEE', 'US', NULL, NULL);
