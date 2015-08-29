57742401e048471a4bff9f310c03edb99ddf5b15
TheMrNomis <n0m1s@homnomnom.fr>
2015-08-29 02:31:26 +0200

    saving the patient info to database

--- process_page.php
+++ process_page.php
@@ -11,7 +11,10 @@ else
     $page = $_GET['page'];
 
 if($page == $page_number - 1)
+{
     saveToDB();
+    session_destroy();
+}
 else
     foreach($_POST as $varName => $value)
         $_SESSION[$varName] = htmlspecialchars($value);
@@ -24,7 +27,35 @@ exit;
 
 function saveToDB()
 {
-    //TODO
     $db = openDB();
+
+    //Patient table
+    $sql = $db->prepare('INSERT INTO Patient (
+            patient_insee, patient_firstName, patient_lastName, patient_dateOfBirth, patient_sex, patient_size, patient_weight,
+            patient_typeOfID, patient_insurancePlanIdentification, patient_countryOfResidence)
+            VALUES (
+            :insee, :firstName, :lastName, :dateOfBirth, :sex, :size, :weight,
+            \'INSEE\', \'INSEE\', :countryOfResidence)
+            ON DUPLICATE KEY UPDATE
+            patient_firstName=:firstName,
+            patient_lastName=:lastName,
+            patient_dateOfBirth=:dateOfBirth,
+            patient_sex=:sex,
+            patient_size=:size,
+            patient_weight=:weight,
+            patient_countryOfResidence=:countryOfResidence;');
+
+    $sql->execute(array(
+        'insee'=>$_SESSION['patient_insee'],
+        'firstName'=>$_SESSION['patient_firstName'],
+        'lastName'=>$_SESSION['patient_lastName'],
+        'dateOfBirth'=>$_SESSION['patient_dateOfBirth'],
+        'sex'=>$_SESSION['patient_sex'],
+        'size'=>$_SESSION['patient_size'],
+        'weight'=>$_SESSION['patient_weight'],
+        'countryOfResidence'=>$_SESSION['patient_countryOfResidence']
+    ));
+
+    //TODO: save to DB for examen
 }
 ?>
